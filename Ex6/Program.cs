7d0c8a66b6021769b1b651fb3a41414536dc522a
bladeshape <tekigod@mail.ru>
2022-08-12 08:23:29 +0300

    ex8done

--- Ex6/Program.cs
+++ Ex6/Program.cs
@@ -7,7 +7,7 @@
 // 7 -> нет
 
 Console.WriteLine("Введите число: ");
- int n1 = Convert.ToInt32(Console.ReadLine());
+int n1 = Convert.ToInt32(Console.ReadLine());
 if ((n1 % 2) == 0)
 {
 Console.WriteLine("Число является четным");
