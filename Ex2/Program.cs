d4c76e6a61ef432982684c96590a2e56618a1f66
bladeshape <tekigod@mail.ru>
2022-08-12 06:14:55 +0300

    ex2d

--- Ex2/Program.cs
+++ Ex2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая 
+// 1. на вход принимает два числа и 
+// 2. выдаёт, какое число большее, а какое меньшее.
+// Пример:
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+ 
+ if(num1 > num2)
+{
+Console.WriteLine("Первое число больше второго");
+}
+else if (num1 < num2)
+Console.WriteLine("Первое число меньше второго");
+
