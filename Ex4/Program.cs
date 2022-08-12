fad8d48db6c797206b2013dc7c2ef98647df406f
bladeshape <tekigod@mail.ru>
2022-08-12 07:05:53 +0300

    ex4dn

--- Ex4/Program.cs
+++ Ex4/Program.cs
@@ -5,3 +5,11 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+
+Console.WriteLine("Введите первое число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Math.Max(n1, Math.Max(n2, n3)));
