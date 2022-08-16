7e13262db8c4c99d7b19360b18cc016c678bf4df
bladeshape <tekigod@mail.ru>
2022-08-16 03:59:34 +0300

    ex10

--- Ex2/Program.cs
+++ Ex2/Program.cs
@@ -1,21 +1,15 @@
 ﻿// Напишите программу, которая 
-// 1. на вход принимает два числа и 
-// 2. выдаёт, какое число большее, а какое меньшее.
-// Например:
-// a = 5; b = 7 -> max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
+// 1. принимает на вход трёхзначное число и 
+// 2. на выходе показывает вторую цифру этого числа.
+// Например: 
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
 
-
-Console.WriteLine("Введите первое число: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
- 
- if(num1 > num2)
-{
-Console.WriteLine("Первое число больше второго");
-}
-else
-Console.WriteLine("Первое число меньше второго");
-
+Console.WriteLine("Введите трехзначное число: ");
+    while(true)
+    {
+        string input = Console.ReadLine();
+        if(!input.Equals("exit"))
+        Console.WriteLine("{0} -> {1}",input, input[1]);
+    }
